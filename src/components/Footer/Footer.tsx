bd2fa4a7778369c8a99b01f4aefe119ef5751873
Jalal Mallah <j.mallah@bsgroup.eu>
2022-09-12 13:01:58 +0200

    feat: update footer.

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,19 +1,29 @@
+/* eslint-disable @next/next/no-img-element */
 import styles from './footer.module.scss';
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <div className={styles.madeBy}>
+      {/* <div className={styles.madeBy}>
         Made by{' '}
         <a href='https://www.jalalmallah.io/' target='_blank' rel='noreferrer'>
           <u>Jalal Mallah</u>
         </a>
-      </div>
+      </div> */}
       <div className={styles.logosContainer}>
         <img src='/ue.jpg' alt='UE' className={styles.footerImage} />
         <img src='/leader.png' alt='Leader' className={styles.footerImage} />
         <img src='/ster.jpg' alt='Ster' className={styles.footerImage} />
         <img src='/prow.jpg' alt='PROW' className={styles.footerImage} />
+      </div>
+      <div className={styles.footerText}>
+        Operacja pn. Wzrost konkurencyjności na rynku województwa łódzkiego
+        poprzez zakup profesjonalnego sprzętu fotograficznego oraz założenia
+        studia fotograficznego współfinansowana jest ze środków Unii
+        Europejskiej w ramach działania 19 Wsparcie dla rozwoju lokalnego w
+        ramach inicjatywy LEADER, poddziałanie 19.2 Wsparcie na wdrażanie
+        operacji w ramach strategii rozwoju lokalnego kierowanego przez
+        społeczność, Programu Rozwoju Obszarów Wiejskich na lata 2014-2020.
       </div>
     </footer>
   );
