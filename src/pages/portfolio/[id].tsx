be0c2bab53027e58131730157bdf1f044883bdc2
Jalal Mallah <j.mallah@bsgroup.eu>
2022-10-23 22:59:31 +0200

    feat: create Category component

--- src/pages/portfolio/[id].tsx
+++ src/pages/portfolio/[id].tsx
@@ -1,10 +1,42 @@
 import { useRouter } from 'next/router';
 
+import PageTitle from 'components/PageTitle';
+
+import styles from './category.module.scss';
+
+const getTitle = (category: string) => {
+  switch (category) {
+    case 'chrzciny':
+      return 'Chrzciny';
+    case 'sluby_i_wesela':
+      return 'Śluby & Wesela';
+    case 'komunie':
+      return 'Komunie';
+    case 'sesje_plenerowe':
+      return 'Sesje Plenerowe';
+    case 'sesje_rodzinne':
+      return 'Sesje Rodzinne';
+    case 'imprezy_okolicznosciowe':
+      return 'Imprezy Okolicznościowe';
+    case 'sesje_romantyczne':
+      return 'Sesje Romantyczne';
+    case 'portrety':
+      return 'Portrety';
+    default:
+      return '';
+  }
+};
+
 const Category = () => {
   const router = useRouter();
-  const { id } = router.query;
+  const { id: category } = router.query;
 
-  return <h2>Kategoria: {id}</h2>;
+  return (
+    <>
+      <PageTitle text={getTitle(category as string)} />
+      <div className={styles.images}>images go here.</div>
+    </>
+  );
 };
 
 export default Category;
